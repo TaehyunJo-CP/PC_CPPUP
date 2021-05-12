0e0e4d9b8afb7722d023c1e9e98e0d935b4a6f39
Tae Hyun Jo <taehyun.jo.90@gmail.com>
2021-05-12 18:31:27 +0900

    lab 2

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -5,73 +5,73 @@ using namespace std;
 
 namespace lab2
 {
-    void PrintIntegers(std::istream& in, std::ostream& out)
-    {
-        out << setw(12) << "oct" << setw(11) << "dec" << setw(9) << "hex" << endl;
-        out << "------------ ---------- --------" << endl;
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		out << setw(12) << "oct" << setw(11) << "dec" << setw(9) << "hex" << endl;
+		out << "------------ ---------- --------" << endl;
 
-        while (true)
-        {
-            int num;
-            
-            in >> num;
+		while (true)
+		{
+			int num;
 
-            if (in.fail())
-            {
-                if (in.eof())
-                {
-                    break;
-                }
+			in >> num;
 
-                in.clear();
-                in.ignore(LLONG_MAX, ' ');
-                continue;
-            }
+			if (in.fail())
+			{
+				if (in.eof())
+				{
+					break;
+				}
 
-            out << setw(12) << oct << num << setw(11) << dec << num << setw(9) << hex << uppercase << num << endl;
+				in.clear();
+				in.ignore(LLONG_MAX, ' ');
+				continue;
+			}
 
-            if (in.eof())
-            {
-                break;
-            }
-        }
-    }
+			out << setw(12) << oct << num << setw(11) << dec << num << setw(9) << hex << uppercase << num << endl;
 
-    void PrintMaxFloat(std::istream& in, std::ostream& out)
-    {
-        float maxNum = FLT_MIN;
+			if (in.eof())
+			{
+				break;
+			}
+		}
+	}
 
-        while (true)
-        {
-            float num;
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float maxNum = FLT_MIN;
 
-            in >> num;
+		while (true)
+		{
+			float num;
 
-            if (in.fail())
-            {
-                if (in.eof())
-                {
-                    break;
-                }
+			in >> num;
 
-                in.clear();
-                in.ignore(LLONG_MAX, ' ');
-                continue;
-            }
+			if (in.fail())
+			{
+				if (in.eof())
+				{
+					break;
+				}
 
-            if (maxNum < num)
-            {
-                maxNum = num;
-            }
+				in.clear();
+				in.ignore(LLONG_MAX, ' ');
+				continue;
+			}
 
-            cout << "     " << setw(15) << internal << fixed << setprecision(3) << num << endl;
+			if (maxNum < num)
+			{
+				maxNum = num;
+			}
 
-            if (in.eof())
-            {
-                break;
-            }
-        }
+			cout << "     " << setw(15) << internal << fixed << setprecision(3) << num << endl;
 
-        cout << "max: " << setw(15) << internal << fixed << setprecision(3) << maxNum << endl;
-    }
+			if (in.eof())
+			{
+				break;
+			}
+		}
+
+		cout << "max: " << setw(15) << internal << fixed << setprecision(3) << maxNum << endl;
+	}
 }
