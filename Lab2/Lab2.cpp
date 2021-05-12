708a8c968551689cd5b18a392e08ac01d9eb0520
Tae Hyun Jo <taehyun.jo.90@gmail.com>
2021-05-12 18:27:00 +0900

    lab 2

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,77 @@
+#include "Lab2.h"
+#include <iomanip>
+
+using namespace std;
+
+namespace lab2
+{
+    void PrintIntegers(std::istream& in, std::ostream& out)
+    {
+        out << setw(12) << "oct" << setw(11) << "dec" << setw(9) << "hex" << endl;
+        out << "------------ ---------- --------" << endl;
+
+        while (true)
+        {
+            int num;
+            
+            in >> num;
+
+            if (in.fail())
+            {
+                if (in.eof())
+                {
+                    break;
+                }
+
+                in.clear();
+                in.ignore(LLONG_MAX, ' ');
+                continue;
+            }
+
+            out << setw(12) << oct << num << setw(11) << dec << num << setw(9) << hex << uppercase << num << endl;
+
+            if (in.eof())
+            {
+                break;
+            }
+        }
+    }
+
+    void PrintMaxFloat(std::istream& in, std::ostream& out)
+    {
+        float maxNum = FLT_MIN;
+
+        while (true)
+        {
+            float num;
+
+            in >> num;
+
+            if (in.fail())
+            {
+                if (in.eof())
+                {
+                    break;
+                }
+
+                in.clear();
+                in.ignore(LLONG_MAX, ' ');
+                continue;
+            }
+
+            if (maxNum < num)
+            {
+                maxNum = num;
+            }
+
+            cout << "     " << setw(15) << internal << fixed << setprecision(3) << num << endl;
+
+            if (in.eof())
+            {
+                break;
+            }
+        }
+
+        cout << "max: " << setw(15) << internal << fixed << setprecision(3) << maxNum << endl;
+    }
+}
